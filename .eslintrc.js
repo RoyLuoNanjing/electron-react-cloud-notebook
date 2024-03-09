60515e97c89df5a54e7918cb14203d2f17692f2a
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-03-09 18:25:04 +0000

    update the configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'error',
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'linebreak-style': 'on',
   },
   parserOptions: {
     ecmaVersion: 2022,
