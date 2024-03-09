a6559f8348ae71068af6a20a81e2d0d7696c4c63
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-03-09 17:24:10 +0000

    web - apply grid

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,17 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Hello() {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+    <div className="Hello container-fluid">
+      <div className="row">
+        <div className="col-3 bg-danger left-panel">
+          <h1>this is the left</h1>
+        </div>
+        <div className="col-9 bg-primary right-panel">
+          <h1>this is the right</h1>
+        </div>
       </div>
     </div>
   );
