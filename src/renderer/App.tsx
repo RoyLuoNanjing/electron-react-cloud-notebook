53ed9663b7c3727231a1f2fec64ac8d68fb703c4
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-03-09 18:25:38 +0000

    update the app

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,5 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { FileSearch } from '../components/FileSearch';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,10 +7,10 @@ function Hello() {
   return (
     <div className="Hello container-fluid">
       <div className="row">
-        <div className="col-3 bg-danger left-panel">
-          <h1>this is the left</h1>
+        <div className="col-4  left-panel">
+          <FileSearch title={'My Cloud Files'} onFileSearch={() => {}} />
         </div>
-        <div className="col-9 bg-primary right-panel">
+        <div className="col-8 bg-primary right-panel">
           <h1>this is the right</h1>
         </div>
       </div>
