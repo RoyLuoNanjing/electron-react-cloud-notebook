7abfd7a021a542f2bb18712f578e420a4b0c971e
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-03-10 09:47:40 +0000

    update the component

--- src/components/FileSearch.tsx
+++ src/components/FileSearch.tsx
@@ -1,15 +1,42 @@
-import { title } from 'process';
-import React, { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 interface IProps {
   title: string;
-  onFileSearch: () => void;
+  onFileSearch: (value: string) => void;
 }
 
 export const FileSearch = (props: IProps) => {
   const { title, onFileSearch } = props;
   const [inputActive, setInputActive] = useState<boolean>(false);
   const [value, setValue] = useState<string>('');
+
+  const closeSearch = (e: any) => {
+    e.preventDefault();
+    setInputActive(false);
+    setValue('');
+  };
+  useEffect(() => {
+    const handleInputEvent = (event: KeyboardEvent) => {
+      const { key } = event;
+      if (key === 'Enter' && inputActive) {
+        onFileSearch(value);
+      } else if (key === 'Escape' && inputActive) {
+        closeSearch(event);
+      }
+    };
+    document.addEventListener('keyup', handleInputEvent);
+    return () => {
+      document.removeEventListener('keyup', handleInputEvent);
+    };
+  });
+
+  let node = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (inputActive) {
+      node.current?.focus();
+    }
+  }, [inputActive]);
+
   return (
     <div className="alert alert-primary">
       {!inputActive && (
@@ -30,11 +57,12 @@ export const FileSearch = (props: IProps) => {
             className="form-control me-2"
             value={value}
             onChange={(e) => setValue(e.target.value)}
+            ref={node}
           />
           <button
             type="button"
             className="btn btn-primary"
-            onClick={() => setInputActive(false)}
+            onClick={closeSearch}
           >
             Close
           </button>
