1bc2cf64e7a5e4b510c657efaca86dff1462b9b6
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-03-23 11:53:42 +0000

    apply the icon interaction logics

--- src/components/TabList.tsx
+++ src/components/TabList.tsx
@@ -2,23 +2,28 @@ import { IFile } from './FileList';
 import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import './TabList.scss';
 
 interface IProps {
   files: IFile[];
   activeId: string;
-  unsavedIds?: string[];
+  unsavedIds: string[];
   onTabClick: (id: string) => void;
-  onCloseTab?: () => void;
+  onCloseTab: (id: string) => void;
 }
 export const TabList = (props: IProps) => {
   const { files, activeId, unsavedIds, onTabClick, onCloseTab } = props;
 
   return (
-    <ul className="nav nav-pills">
+    <ul className="nav nav-pills tablist-component">
       {files.map((file) => {
+        const withUnsavedMark = unsavedIds.includes(file.id);
+
         const fClassname = classNames({
           'nav-link': true,
           active: file.id === activeId,
+          // prettier-ignore
+          'withUnsaved': withUnsavedMark,
         });
         return (
           <li className="nav-item" key={file.id}>
@@ -31,9 +36,21 @@ export const TabList = (props: IProps) => {
               }}
             >
               {file.title}
-              <span style={{ marginLeft: '4px' }}>
-                <FontAwesomeIcon icon={faTimes} />
+              <span style={{ marginLeft: '4px' }} className="close-icon">
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  onClick={(e) => {
+                    e.stopPropagation(); //阻止冒泡
+                    onCloseTab(file.id);
+                  }}
+                />
               </span>
+              {withUnsavedMark && (
+                <span
+                  className="rounded-circle unsaved-icon "
+                  style={{ marginLeft: '4px' }}
+                ></span>
+              )}
             </a>
           </li>
         );
