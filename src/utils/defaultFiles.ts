e994b5d75db4ac9fd852423e94c88ceb217ca8b1
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-03-23 15:50:37 +0000

    update default values

--- src/utils/defaultFiles.ts
+++ src/utils/defaultFiles.ts
@@ -4,24 +4,28 @@ const defaultFiles = [
     title: 'first post',
     body: '*should be aware of this*',
     createdAt: 1563762965704,
+    isNew: false,
   },
   {
     id: '2',
     title: 'second post',
     body: '## this is the title',
     createdAt: 1563762965704,
+    isNew: false,
   },
   {
     id: '3',
     title: '你好世界',
     body: '### 这是另外一个小标题欧',
     createdAt: 1563762965704,
+    isNew: false,
   },
   {
     id: '4',
     title: '你好世界',
     body: '**粗体内容**',
     createdAt: 1563762965704,
+    isNew: false,
   },
 ];
 
