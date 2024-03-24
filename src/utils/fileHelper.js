03c6eba84cde626f850c2bba57a47e0cb8c6d431
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-03-24 15:22:02 +0000

    update

--- src/utils/fileHelper.js
+++ src/utils/fileHelper.js
@@ -1,13 +1,11 @@
-const fs = require('fs').promises;
-const path = require('path');
+const fs = window.require('fs').promises;
+const path = window.require('path');
 
 const fileHelper = {
   readFile: (path) => {
-    //@ts-ignore
     return fs.readFile(path, { encoding: 'utf8' });
   },
   writeFile: (path, content) => {
-    //@ts-ignore
     return fs.writeFile(path, content, { encoding: 'utf8' });
   },
   renameFile : (path, newPath)=>{
@@ -18,23 +16,5 @@ const fileHelper = {
     return fs.unlink(path)
   }
 };
-const testPath = path.join(__dirname, 'helper.js');
-const testWritePath = path.join(__dirname, 'hello.md');
-const renamePath = path.join(__dirname, 'rename.md');
 
-
-// fileHelper.readFile(testPath).then((data) => {
-//   console.log(data);
-// });
-
-// fileHelper.writeFile(testWritePath, '## hello world').then(() => {
-//   console.log('write in successfully');
-// });
-
-// fileHelper.renameFile(testWritePath, renamePath).then(() => {
-//   console.log('rename successfully');
-// });
-
-fileHelper.deleteFile( renamePath).then(() => {
-  console.log('delete successfully');
-});
+export default fileHelper
