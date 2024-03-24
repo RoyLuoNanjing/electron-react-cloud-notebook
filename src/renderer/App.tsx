c2c945fb5b16c975c85ba6e4525d752e2604cc96
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-03-24 17:38:44 +0000

    use electron-store

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -9,10 +9,9 @@ import {
   faSave,
 } from '@fortawesome/free-solid-svg-icons';
 import { v4 as uuidv4 } from 'uuid';
-import { flattenArr, objToArr } from '../utils/helper';
+import { objToArr } from '../utils/helper';
 import SimpleMde from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
-import defaultFiles from '../utils/defaultFiles';
 import { BottomBtn } from '../components/BottomBtn';
 import { TabList } from '../components/TabList';
 import { useState } from 'react';
@@ -21,8 +20,26 @@ import fileHelper from '../utils/fileHelper';
 // require node.js modules
 const { join } = window.require('path');
 const remote = window.require('@electron/remote');
+const Store = window.require('electron-store');
+
+const fileStore = new Store({ name: 'Files Data' });
+const saveFilesToStore = (files: IFile) => {
+  //we don't have to store any info in file system, eg: isNew, BODY, ETC
+  const filesStoreObj = objToArr(files).reduce((result, file) => {
+    const { id, path, title, createdAt } = file;
+    result[id] = {
+      id,
+      path,
+      title,
+      createdAt,
+    };
+    return result;
+  }, {});
+  fileStore.set('files', filesStoreObj);
+};
+
 function Hello() {
-  const [files, setFiles] = useState(flattenArr(defaultFiles));
+  const [files, setFiles] = useState(fileStore.get('files') || {});
   const [activeFileID, setActiveFileID] = useState('');
   const [openedFileIDs, setOpenFileIDs] = useState<string[]>([]);
   const [unsavedFileIDs, setUnsavedFileIDs] = useState<string[]>([]);
@@ -72,37 +89,34 @@ function Hello() {
   };
 
   const deleteFile = (id: string) => {
-    fileHelper
-      .deleteFile(join(savedLocation, `${files[id].title}.md`))
-      .then(() => {
-        const { [id]: value, ...newFiles } = files;
-        setFiles(newFiles);
-      });
-    //  filter out the current file id
-    delete files[id];
-    setFiles(files);
-    //close the tab if opened
-    tabClose(id);
+    fileHelper.deleteFile(join(files[id].path)).then(() => {
+      //  filter out the current file id
+      delete files[id];
+      setFiles(files);
+      saveFilesToStore(files);
+      //close the tab if opened
+      tabClose(id);
+    });
   };
 
   const updateFileName = (id: string, title: string, isNew: boolean) => {
+    const newPath = join(savedLocation, `${title}.md`);
     //loop through files, and update the title
-    const modifiedFile = { ...files[id], title, isNew: false };
+    const modifiedFile = { ...files[id], title, isNew: false, path: newPath };
+
+    const newFiles = { ...files, [id]: modifiedFile };
+
     if (isNew) {
-      fileHelper
-        .writeFile(join(savedLocation, `${title}.md`), files[id].body)
-        .then(() => {
-          setFiles({ ...files, [id]: modifiedFile });
-        });
+      fileHelper.writeFile(newPath, files[id].body).then(() => {
+        setFiles(newFiles);
+        saveFilesToStore(newFiles);
+      });
     } else {
-      fileHelper
-        .renameFile(
-          join(savedLocation, `${files[id].title}.md`),
-          join(savedLocation, `${title}.md`),
-        )
-        .then(() => {
-          setFiles({ ...files, [id]: modifiedFile });
-        });
+      const oldPath = join(savedLocation, `${files[id].title}.md`);
+      fileHelper.renameFile(oldPath, newPath).then(() => {
+        setFiles(newFiles);
+        saveFilesToStore(newFiles);
+      });
     }
   };
 
