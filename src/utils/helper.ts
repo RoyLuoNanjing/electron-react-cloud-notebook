f4ccbe6051af314dc3ca158218b6d06663de58c7
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-03-27 18:38:35 +0000

    add an util

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -9,3 +9,15 @@ export const flattenArr = (arr: any) => {
 export const objToArr = (obj: any) => {
   return Object.keys(obj).map((key) => obj[key]);
 };
+
+export const getParentNode = (node: any, parentClassName: any) => {
+  let current = node;
+  while (current !== null) {
+    if (current.classList.contains(parentClassName)) {
+      return current;
+    }
+
+    current = current.parentNode;
+  }
+  return false;
+};
